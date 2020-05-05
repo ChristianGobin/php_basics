43ccc6d810ff3f92e24b56df6bfe100b2985d2fc
Christian G <chrisgobin94@gmail.com>
2020-05-04 21:19:05 -0400

    Using Php Variables in HTML

--- basic_1.php
+++ basic_1.php
@@ -3,14 +3,32 @@
 //Variables
 $var = "This is a String ";
 $intVar = 4;
-
-echo $var;
-echo $intVar;
+//set title of page in head to $title var
+$title = "Basic PHP";
+//set heading of html body using php variable
+$heading = "<h1>PHP Heading Variable</h1>";
 //Data Types
 
 
-
-
-
-
 //Built in methods, manipulating data
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo $title ?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Bootstrap 4 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <?php echo $heading ?>
+</body>
+
+</html>
